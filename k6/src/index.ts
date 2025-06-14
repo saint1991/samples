47aa744f3a7e3e7053de2e045f14d130a122be76
saint1991 <se.mizuno.tech@gmail.com>
2025-06-14 21:06:40 +0900

    use TypeScript directly (#28)

--- k6/src/index.ts
+++ k6/src/index.ts
@@ -1,8 +1,8 @@
 /*eslint import/no-unresolved: [2, { ignore: ['^k6.*'] }]*/
 
-import { SubjectApi } from "./libs/apis/subject";
+import { SubjectApi } from "./libs/apis/subject.ts";
 
-export default  () => {
-    const api = new SubjectApi("http://localhost:8888");
-    api.createSubject("新しいサブジェクト");
+export default () => {
+	const api = new SubjectApi("http://localhost:8888");
+	api.createSubject("新しいサブジェクト");
 };
