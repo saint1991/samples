3437b67f4f1862d222ac7f94299f23da38854b97
saint1991 <se.mizuno.tech@gmail.com>
2025-10-05 00:02:06 +0900

    init duckdb-wasm table sample

--- typescript/ag-duck-table/src/app/app.config.ts
+++ typescript/ag-duck-table/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
@@ -7,6 +7,7 @@ import { provideClientHydration, withEventReplay, withIncrementalHydration } fro
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay(), withIncrementalHydration()),
--- typescript/ag-duck-table/src/app/app.routes.ts
+++ typescript/ag-duck-table/src/app/app.routes.ts
@@ -2,10 +2,20 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { App } from './app';
 import { Profile } from './profile/profile';
+import { TableComponent } from './table/table.component';
+import { PivotTableComponent } from './pivot-table/pivot-table.component';
 
 export const routes: Routes = [
   {
     path: '',
+    component: PivotTableComponent
+  },
+  {
+    path: 'table',
+    component: TableComponent
+  },
+  {
+    path: 'home',
     component: App
   },
   {
