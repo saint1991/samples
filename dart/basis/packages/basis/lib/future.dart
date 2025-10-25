1854bd0c0e140db96840e7983b899d16178fec83
saint1991 <se.mizuno.tech@gmail.com>
2025-10-26 02:03:55 +0900

    add stream examples

--- dart/basis/packages/basis/lib/future.dart
+++ dart/basis/packages/basis/lib/future.dart
@@ -29,5 +29,41 @@ Stream<String> fileChunks(String filePath) {
 }
 
 /// async*キーワードを使うことで、ジェネレータっぽい関数を定義できる。
-/// Streamが
-Stream<String> generatorLike() async* {}
+/// listenされて購読された時に初めて実行される。
+/// ///
+Stream<String> generatorLike(String filePath) async* {
+  await Future.delayed(const Duration(seconds: 1));
+
+  //yieldで単体の値を送出
+  yield "Start reading $filePath";
+
+  // yield*で別のStreamに連結できる
+  yield* fileChunks(filePath);
+
+  throw Exception("Intentional exception");
+
+  yield "End of $filePath";
+}
+
+class StreamControllerExample {
+  // StreamControllerはSubjectに近い
+  // 値はバッファリングされる
+  final StreamController<String> _controller = StreamController<String>();
+
+  Stream<String> get stream => _controller.stream;
+
+  final List<String> _values;
+  int _index;
+
+  StreamControllerExample(List<String> values) : _values = values, _index = 0;
+
+  void next() {
+    if (_index < _values.length) {
+      // addでstreamに値を流す
+      _controller.add(_values[_index]);
+      _index += 1;
+    } else {
+      _controller.close();
+    }
+  }
+}
